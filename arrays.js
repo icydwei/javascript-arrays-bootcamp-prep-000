4cd655e09084a047df874b6935eae3fcd3522abb
Rob Paik <robpaik@gmail.com>
2019-06-16 02:13:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,7 @@ function destructivelyAddElementToEndOfArray (array, element) {
 function accessElementInArray (array, element) {
   return array[element];
 }
+
+function destructivelyRemoveElementFromBeginningofArray(array, element) {
+  array.shift()
+}
